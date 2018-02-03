74c828ca1f21ae7c6381e1d34d74bba11476ce6a
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2018-02-03 02:58:34 +0000

    Add notes from file i/o reading

--- file_io.rb
+++ file_io.rb
@@ -1,4 +1,29 @@
+# batch of notes from file i/o reading
+
+
 fname = "sample.txt"
 somefile = File.open(fname, "w")
 somefile.puts "Hello file!"
 somefile.close
+
+# same thing happens, block implicitly closes
+File.open("sample.txt", "w") { |somefile| somefile.puts "Hello file!" }
+
+file = File.open("sample.txt", "r")
+contents = file.read
+puts contents
+
+block format for read
+contents = File.open("sample.txt", "r") { |file| file.read }
+puts contents
+
+# readlines vs readline methods
+File.open("sample.txt").readlines.each do |line|
+  puts line
+end
+
+file = File.open("sample.txt", 'r')
+while !file.eof?
+  line = file.readline
+  puts line
+end
